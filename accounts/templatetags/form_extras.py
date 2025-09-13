023577ed4f54d0a1d7f0168d6763a90f2ff7576f
DoctorWallace <74184371+DoctorWallace@users.noreply.github.com>
2025-09-14 00:18:38 +0200

    Templatetags (accounts): añadir filtro add_attr y mejorar add_class para preservar clases

--- accounts/templatetags/form_extras.py
+++ accounts/templatetags/form_extras.py
@@ -1,5 +1,7 @@
 from django import template
+
 register = template.Library()
+
 
 @register.filter(name="has_group")
 def has_group(user, group_name: str):
@@ -8,6 +10,20 @@ def has_group(user, group_name: str):
     except Exception:
         return False
 
-@register.filter
-def add_class(field, css):
-    return field.as_widget(attrs={"class": css})
+
+@register.filter(name="add_class")
+def add_class(field, css: str):
+    # Add classes preserving existing ones
+    existing = field.field.widget.attrs.get("class", "")
+    new_classes = (existing + " " + str(css)).strip()
+    return field.as_widget(attrs={**field.field.widget.attrs, "class": new_classes})
+
+
+@register.filter(name="add_attr")
+def add_attr(field, arg: str):
+    # Set/update an attribute using "name:value"
+    try:
+        key, value = str(arg).split(":", 1)
+    except ValueError:
+        return field
+    return field.as_widget(attrs={**field.field.widget.attrs, key: value})
